10e1a469e125fb623d66c4bbd3ce467c0cad99bf
NithyasreeS11 <119521753+NithyasreeS11@users.noreply.github.com>
2025-06-05 19:48:38 +0530

    Create MoveZeros.java

---
+++ MoveZeros.java
@@ -0,0 +1,13 @@
+class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i]==0){
+                for(int j=i;j<nums.length-1;j++){
+                    nums[j] = nums[j+1];
+                }
+                nums[nums.length-1] = 0;
+            }
+        }
+        
+    }
+}
