94b66cc11973d56f3417ccc9a62e2ac039534ce5
NithyasreeS11 <119521753+NithyasreeS11@users.noreply.github.com>
2025-06-06 12:24:44 +0530

    Create MergeIntervals.java

---
+++ MergeIntervals.java
@@ -0,0 +1,20 @@
+class MergeIntervals {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) return intervals;
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        List<int[]> merged = new ArrayList<>();
+        int[] current = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            int[] next = intervals[i];
+            if (current[1] >= next[0]) {
+                current[1] = Math.max(current[1], next[1]);
+            } 
+            else {
+                merged.add(current); 
+                current = next;      
+            }
+        }
+        merged.add(current);
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
